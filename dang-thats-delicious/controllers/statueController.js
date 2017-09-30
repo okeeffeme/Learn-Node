e69712fae98f3762d582167856bfe3c77e57023a
okeeffeme <okeeffeme@gmail.com>
2017-09-30 15:30:57 +0200

    Adding mongoose to statue creation form

--- dang-thats-delicious/controllers/statueController.js
+++ dang-thats-delicious/controllers/statueController.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
+const Statue = mongoose.model('Statue');
+
 exports.homepage = (req, res) => {
   res.render('index');
 };
 
 exports.addStatue = (req, res) => {
   res.render('editStatue', {title: 'Add Statue'});
+};
+
+exports.createStatue = async (req, res) => {
+  const statue = new Statue(req.body);
+  await statue.save();
+  console.log('It worked!');
+  res.redirect('/');
 };
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const statueController = require('../controllers/statueController')
+const statueController = require('../controllers/statueController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', statueController.homepage);
 router.get('/add', statueController.addStatue);
+router.post('/add', catchErrors(statueController.createStatue));
 
 module.exports = router;
